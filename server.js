5923375f2ab001b5aa4758f805efb32af5020566
Arsenii Bylym <arseniibylym@gmail.com>
2019-08-22 21:30:25 +0300

    Add server.js and run local server

---
+++ server.js
@@ -0,0 +1,45 @@
+const path = require(`path`);
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const cors = require('cors');
+require('dotenv').config();
+
+const MONGO_DB_URI = process.env.MONGO_DB_URI;
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+// Middlewares
+app.use(helmet());
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(cors());
+
+// Main routes
+
+
+// Error handling
+app.use((err, req, res, next) => {
+    console.log(red(err));
+    const {statusCode = 500, message, errors} = err;
+    return res.status(statusCode).json({message, errors});
+});
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+mongoose
+    .connect(MONGO_DB_URI, {useNewUrlParser: true})
+    .then(() => {
+        app.listen(PORT);
+        console.log(`Server listen on port ${PORT}`);
+    })
+    .catch(error => {
+        console.log('Connection error')
+        console.log(error);
+    });
