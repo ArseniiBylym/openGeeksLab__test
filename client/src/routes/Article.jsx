8861cd8ab418d6fcd69835aafcb9abf7ea0d76d0
Arsenii Bylym <arseniibylym@gmail.com>
2019-08-25 10:07:41 +0300

    Article and recipe details pages

--- client/src/routes/Article.jsx
+++ client/src/routes/Article.jsx
@@ -1,13 +1,52 @@
 import React, {useState, useEffect} from 'react';
 import styles from './Article.module.scss';
+import {fetchApi, URL_PATH} from '../api';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import {Spinner, MainHeader} from '../components/shared';
+import {ArticleModal} from '../components/modals';
+import defaultImage from '../assets/images/article.jpg';
 
 const Article = props => {
+    const {id} = props.match.params;
+    const [article, setArticle] = useState(null);
+    useEffect(() => {
+        fetchArticle();
+    }, [id]);
 
+    const fetchArticle = async () => {
+        const response = await fetchApi.get(`${URL_PATH.ARTICLES}/${id}`);
+        setArticle(response.data);
+    };
+    
+    const onDeleteHandler = async () => {
+
+    }
+
+    if (!article) return <Spinner />;
     return (
         <div className={styles.root}>
-            Article
+            <MainHeader />
+            <div className={styles.content}>
+                <Typography variant="h3" align="center" gutterBottom>
+                    {article.title}
+                </Typography>
+                <Typography variant="h4" color="textSecondary" align="center" gutterBottom>
+                    {article.subTitle}
+                </Typography>
+                <img className={styles.image} src={article.imageUrl || defaultImage} />
+                <Typography variant="body1" align="center" gutterBottom className={styles.text}>
+                    {article.text}
+                </Typography>
+                <div className={styles.controls}>
+                    <ArticleModal id={article._id} />
+                    <Button color="secondary" variant="contained" onClick={onDeleteHandler}>
+                        Delete
+                    </Button>
+                </div>
+            </div>
         </div>
-    )
+    );
 };
 
-export default Article
+export default Article;
--- client/src/routes/Recipe.jsx
+++ client/src/routes/Recipe.jsx
@@ -1,13 +1,48 @@
 import React, {useState, useEffect} from 'react';
 import styles from './Recipe.module.scss';
+import {fetchApi, URL_PATH} from '../api';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import {Spinner, MainHeader} from '../components/shared';
+import {RecipeModal} from '../components/modals';
+import defaultImage from '../assets/images/recipe.jpg';
 
 const Recipe = props => {
+    const {id} = props.match.params;
+    const [recipe, setRecipe] = useState(null);
 
+    useEffect(() => {
+        fetchRecipe();
+    }, [id]);
+
+    const fetchRecipe = async () => {
+        const response = await fetchApi.get(`${URL_PATH.RECIPES}/${id}`);
+        setRecipe(response.data);
+    };
+
+    const onDeleteHandler = async () => {};
+
+    if (!recipe) return <Spinner />;
     return (
         <div className={styles.root}>
-            Recipe
+            <MainHeader />
+            <div className={styles.content}>
+                <Typography variant="h3" align="center" gutterBottom>
+                    {recipe.title}
+                </Typography>
+                <img className={styles.image} src={recipe.imageUrl || defaultImage} />
+                <Typography variant="body1" align="center" gutterBottom className={styles.text}>
+                    {recipe.text}
+                </Typography>
+                <div className={styles.controls}>
+                    <RecipeModal id={recipe._id} />
+                    <Button color="secondary" variant="contained" onClick={onDeleteHandler}>
+                        Delete
+                    </Button>
+                </div>
+            </div>
         </div>
-    )
+    );
 };
 
-export default Recipe
+export default Recipe;
